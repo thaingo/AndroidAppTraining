c13cebeebf2b5263cf0b2ea87c609cf5cbb59d3f
thaingo <baothaingo@gmail.com>
2018-07-12 17:39:36 +0700

    starting activity

--- app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.thaingo.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
+
+  public static final String EXTRA_MESSAGE = "com.example.thaingo.myapplication.MESSAGE";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
+
+  public void sendMessage(View view) {
+    Intent intent = new Intent(this, DisplayMessageActivity.class);
+    EditText editText = findViewById(R.id.editText);
+    String message = editText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+
+    startActivity(intent);
   }
 }
