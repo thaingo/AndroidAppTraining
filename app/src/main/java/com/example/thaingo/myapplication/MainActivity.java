95e4e5a8cd9434f55246949767ac7c4160103dd1
thaingo <baothaingo@gmail.com>
2018-07-18 18:24:16 +0700

    basic gridview

--- app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
@@ -1,30 +1,31 @@
 package com.example.thaingo.myapplication;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    GridView gridView = new GridView(this);
+    setContentView(gridView);
+
     String[] countries = new String[]{"Vietnam", "Russia", "US", "Malaysia", "Japan"};
 
-    ListView listView = getListView();
-
-    listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     ListAdapter countriesAdapter =
-        new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, countries);
-    listView.setAdapter(countriesAdapter);
-    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+    gridView.setAdapter(countriesAdapter);
+    gridView.setNumColumns(2);
+    gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         String s = ((TextView) view).getText() + " " + position;
