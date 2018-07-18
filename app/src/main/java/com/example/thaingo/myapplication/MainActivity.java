8cfff7d83fd42cbb92848a882af7c3ea6cb7b02f
thaingo <baothaingo@gmail.com>
2018-07-18 18:08:53 +0700

    multiple choices list view

--- app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
@@ -6,28 +6,25 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ListActivity {
-  /*ListView listView;
-  String[] countries = new String[]{"Vietnam", "Russia", "US", "Malaysia", "Japan"};*/
-  /*int[] flagId =
-      new int[]{R.drawable.flag_vn, R.drawable.flag_ru, R.drawable.flag_us, R.drawable.flag_my, R.drawable.flag_jp};*/
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     String[] countries = new String[]{"Vietnam", "Russia", "US", "Malaysia", "Japan"};
 
-   /* setContentView(R.layout.activity_main);
-    listView = findViewById(R.id.listCountries);*/
+    ListView listView = getListView();
 
+    listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     ListAdapter countriesAdapter =
-        new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
-    setListAdapter(countriesAdapter);
-    getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        new ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, countries);
+    listView.setAdapter(countriesAdapter);
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override
       public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         String s = ((TextView) view).getText() + " " + position;
