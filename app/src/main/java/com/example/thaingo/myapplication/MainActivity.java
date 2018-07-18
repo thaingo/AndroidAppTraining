ca604fe1ee88687a59543a8087cf07db192537c3
thaingo <baothaingo@gmail.com>
2018-07-18 11:00:27 +0700

    saved an activity's state

--- app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/thaingo/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
   public static final String EXTRA_MESSAGE = "com.example.thaingo.myapplication.MESSAGE";
   public static final String REQUEST_RESULT = "REQUEST_RESULT";
+  public static final String KEY_COUNTER = "COUNTER";
+
+  private int count = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +45,23 @@ public class MainActivity extends AppCompatActivity {
     Intent intent = new Intent(Intent.ACTION_VIEW);
     intent.setData(Uri.parse("https://www.google.com.vn"));
     startActivity(intent);
+  }
+
+  public void onClickCounter(View view) {
+    count++;
+    TextView textView = findViewById(R.id.textView);
+    textView.setText("Counter: " + Integer.toString(count));
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    super.onSaveInstanceState(outState);
+    outState.putInt(KEY_COUNTER, count);
+  }
+
+  @Override
+  protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    super.onRestoreInstanceState(savedInstanceState);
+    count = savedInstanceState.getInt(KEY_COUNTER);
   }
 }
